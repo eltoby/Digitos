e69fdc8b450696c502d8b7e462f3df034dd7ad9d
Pablo Rausch <pablorausch@gmail.com>
2017-01-17 00:35:36 -0300

    Interfaz

--- Digitos/Digitos.Console/Program.cs
+++ Digitos/Digitos.Console/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Digitos.Console
+﻿namespace Digitos.Console
 {
+    using System;
+    using Domain;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var generador = new Generador();
+            var buscador = new Buscador(generador);
+
+            var numero = "";
+            while (numero != "X")
+            {
+                Console.WriteLine("Ingrese el número a buscar (X para salir): ");
+                numero = Console.ReadLine();
+
+                if (numero == "X")
+                    return;
+
+                var result = buscador.Buscar(int.Parse(numero));
+
+                Console.WriteLine($"El número está en la posición {result}");
+            }
         }
     }
 }
--- Digitos/Digitos.Tests/IntegrationTests.cs
+++ Digitos/Digitos.Tests/IntegrationTests.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Digitos.Tests
+﻿namespace Digitos.Tests
 {
-    class IntegrationTests
+    using Domain;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class IntegrationTests
     {
+        private IBuscador buscador;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var generador = new Generador();
+            this.buscador = new Buscador(generador);
+
+        }
+
+        [TestMethod]
+        public void Test15()
+        {
+            var result = this.buscador.Buscar(15);
+            Assert.AreEqual(20, result);
+        }
+
+        [TestMethod]
+        public void Test34()
+        {
+            var result = this.buscador.Buscar(34);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Test142()
+        {
+            var result = this.buscador.Buscar(142);
+            Assert.AreEqual(73, result);
+        }
     }
 }
